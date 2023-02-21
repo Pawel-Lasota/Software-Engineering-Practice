a8b117452b9dee65c0fabab49bc638f927963baf
pawel <pawellasota2002@gmail.com>
2023-02-21 01:42:32 +0000

    Adding more classes - structuring

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("This will be the training & test class")
+from time import time
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import fetch_lfw_people
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+import os
+
+
