e7e3feccfb8c48e392b94223269b141ef5056788
pawel <pawellasota2002@gmail.com>
2023-02-24 18:38:47 +0000

    resizing images + converting to grayscale + normalising + saving them into new folder + face detection + detecting face from random image + train and split

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import glob
+import pathlib
 import random
 import shutil
 from tkinter import Image
 import os
-
 import cv2
 import mtcnn
 from matplotlib import pyplot, pyplot as plt
@@ -34,9 +34,9 @@ def normalize_image(image):
 def save_image(image, directory, filename):
     cv2.imwrite(os.path.join(directory, filename), image)
 
-directory = input("Enter the directory to process: ")
 def process_images():
     # Get the directory to process
+    directory = input("Enter the directory to process: ")
     people_count = len(os.listdir(directory))
     print("Found images of", people_count, "people/classes/labels in this directory")
 
@@ -45,6 +45,10 @@ def process_images():
 
     # Size to resize the images
     img_width, img_height = 200, 200
+
+    # Initialize variables to keep track of the current person and folder
+    current_person = None
+    current_folder = None
 
     # Iterate through the directory
     for root, _, files in os.walk(directory):
@@ -58,9 +62,18 @@ def process_images():
             # Get the extension
             extension = os.path.splitext(filepath)[1]
 
-
             # Check if the file is an image
             if extension in [".jpg", ".jpeg", ".png"]:
+                # Get the person's name from the filepath
+                person = os.path.basename(root)
+
+                # Check if we've encountered a new person
+                if person != current_person:
+                    # If so, create a new folder for the person's images
+                    current_person = person
+                    current_folder = os.path.join(save_directory, person)
+                    os.makedirs(current_folder, exist_ok=True)
+
                 # Load the image
                 image = mpimg.imread(filepath)
 
@@ -73,9 +86,9 @@ def process_images():
                 # Normalize the image
                 image = normalize_image(image)
 
-                # Save the image
-                print("Saving image named", filename,"to new folder...")
-                #save_image(image, save_directory, filename)
+                # Save the image in the current person's folder
+                print("Saving image named", filename, "to", current_folder)
+                #save_image(image, current_folder, filename)
 
 
 if __name__ == "__main__":
@@ -119,26 +132,34 @@ plt.imshow(pixels)
 plt.show()
 print(pixels.shape)
 
-def train_and_split(percentage):
-    # Get data
-    train_index, test_index = {}, {}
-    n_train, n_test = 0, 0
-    total = 0
-    for name in os.listdir(directory):
-        path = directory + "/" + name
-        n = len(os.listdir(path))
-        total += n
-        if np.random.rand() < percentage:
-            train_index[name] = n
-            n_train += 1
-        else:
-            test_index[name] = n
-            n_test += 1
-    print("Found a total of", total, "images.")
-    return n_train, train_index, n_test, test_index
+"""def load_face(dir):
+    faces = list()
+    # enumerate files
+    for filename in os.listdir(dir):
+        path = dir + filename
+        face = extract_face(path)
+        faces.append(face)
+    return faces
 
-    # Run the previous function
-n_train, train_list, n_test, test_list = train_and_split(0.8)
-print("Split the people into", n_train, "for training and", n_test, "for testing.")
+def load_dataset(dir):
+    # list for faces and labels
+    X, y = list(), list()
+    for subdir in os.listdir(dir):
+        path = dir + subdir + '/'
+        faces = load_face(path)
+        labels = [subdir for i in range(len(faces))]
+        print("loaded %d sample for class: %s" % (len(faces),subdir) ) # print progress
+        X.extend(faces)
+        y.extend(labels)
+    return np.asarray(X), np.asarray(y)
+
+trainX, trainy = load_dataset('D:\\pyCharm Projects Python\\database\\new_train\\')
+print(trainX.shape, trainy.shape)
+# load test dataset
+testX, testy = load_dataset('D:\\pyCharm Projects Python\\database\\new_train\\')
+print(testX.shape, testy.shape)
+
+np.savez_compressed('classes_pins_dataset_train.npz', trainX, trainy, testX, testy)"""
+
 
 
