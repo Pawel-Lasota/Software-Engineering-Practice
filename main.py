669ceed05243bb0c180f3fb7d5458d66a61c615f
pawel <pawellasota2002@gmail.com>
2023-02-24 19:51:10 +0000

    resizing images + converting to grayscale + normalising + saving them into new folder + face detection + detecting face from random image + train and split

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ from matplotlib import pyplot, pyplot as plt
 from mtcnn import MTCNN
 from PIL import Image
 import numpy as np
+import tensorflow as tf
 from numpy import asarray
+import face_recognition
 import matplotlib.image as mpimg
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 # example of creating a face embedding
 
@@ -34,97 +37,20 @@ def normalize_image(image):
 def save_image(image, directory, filename):
     cv2.imwrite(os.path.join(directory, filename), image)
 
+
 def get_random_image(root_directory):
     # Get a random subdirectory
-    subdirectories = [os.path.join(root_directory, d) for d in os.listdir(root_directory) if os.path.isdir(os.path.join(root_directory, d))]
+    subdirectories = [os.path.join(root_directory, d) for d in os.listdir(root_directory) if
+                      os.path.isdir(os.path.join(root_directory, d))]
     subdirectory = random.choice(subdirectories)
 
     # Get a random image from the subdirectory
-    images = [os.path.join(subdirectory, f) for f in os.listdir(subdirectory) if os.path.isfile(os.path.join(subdirectory, f))]
+    images = [os.path.join(subdirectory, f) for f in os.listdir(subdirectory) if
+              os.path.isfile(os.path.join(subdirectory, f))]
     image_path = random.choice(images)
 
     return image_path
-def process_images():
-    # Get the directory to process
-    directory = input("Enter the directory to process: ")
-    people_count = len(os.listdir(directory))
-    print("Found images of", people_count, "people/classes/labels in this directory")
 
-    # Get the directory to save the images
-    save_directory = input("Enter the directory to save the images (choose different than previous): ")
-
-    # Size to resize the images
-    img_width, img_height = 200, 200
-
-    # Initialize variables to keep track of the current person and folder
-    current_person = None
-    current_folder = None
-
-    # Initialize lists to hold the training and testing data
-    training_data = []
-    testing_data = []
-
-    # Iterate through the directory
-    for root, _, files in os.walk(directory):
-        for file in files:
-            # Get the filepath
-            filepath = os.path.join(root, file)
-
-            # Get the filename
-            filename = os.path.basename(filepath)
-
-            # Get the extension
-            extension = os.path.splitext(filepath)[1]
-
-            # Check if the file is an image
-            if extension in [".jpg", ".jpeg", ".png"]:
-                # Get the person's name from the filepath
-                person = os.path.basename(root)
-
-                # Check if we've encountered a new person
-                if person != current_person:
-                    # If so, create a new folder for the person's images
-                    current_person = person
-                    current_folder = os.path.join(save_directory, person)
-                    os.makedirs(current_folder, exist_ok=True)
-
-                # Load the image
-                image = mpimg.imread(filepath)
-
-                # Resize the image
-                image = resize_image(image, (img_width, img_height))
-
-                # Convert the image to grayscale
-                image = convert_to_grayscale(image)
-
-                # Normalize the image
-                image = normalize_image(image)
-
-                # Randomly split the data into training and testing sets
-                if random.random() < 0.8:
-                    # Save the image in the current person's training folder
-                    training_data.append((image, current_folder))
-                else:
-                    # Save the image in the current person's testing folder
-                    testing_data.append((image, current_folder))
-
-    # Shuffle the training and testing data
-    random.shuffle(training_data)
-    random.shuffle(testing_data)
-
-    # Save the training data
-    for i, (image, folder) in enumerate(training_data):
-        filename = f"train_{i}.jpg"
-        save_image(image, folder, filename)
-
-    # Save the testing data
-    for i, (image, folder) in enumerate(testing_data):
-        filename = f"test_{i}.jpg"
-        save_image(image, folder, filename)
-
-
-if __name__ == "__main__":
-    process_images()
 
 # extract a single face from a given photograph
 def extract_face(filename, required_size=(200, 200)):
@@ -138,6 +64,10 @@ def extract_face(filename, required_size=(200, 200)):
     detector = MTCNN()
     # detect faces in the image
     results = detector.detect_faces(pixels)
+
+    if not results:
+        return None
+
     # extract the bounding box from the first face
     x1, y1, width, height = results[0]['box']
     # deal with negative pixel index
@@ -151,11 +81,73 @@ def extract_face(filename, required_size=(200, 200)):
     face_array = np.asarray(image)
     return face_array
 
-# load the photo and extract the face
-directory_detection = input("From which directory would you like to detect a face?")
-print("Detecting a random images' face...")
-pixels = extract_face(get_random_image(directory_detection))
-plt.imshow(pixels)
-plt.show()
-print(pixels.shape)
 
+def process_images():
+    # Get the directory to process
+    directory = input("Enter the directory to process: ")
+    people_count = len(os.listdir(directory))
+    print("Found images of", people_count, "people/classes/labels in this directory")
+
+    # Get the directory to save the images
+    save_directory = input("Enter the directory to save the images (choose different than previous): ")
+
+    # Create the overall train and test directories
+    train_dir = os.path.join(save_directory, "train")
+    test_dir = os.path.join(save_directory, "test")
+    os.makedirs(train_dir, exist_ok=True)
+    os.makedirs(test_dir, exist_ok=True)
+
+    # Size to resize the images
+    img_width, img_height = 200, 200
+
+    # Iterate through the directory
+    for root, _, files in os.walk(directory):
+        # Ignore the root directory
+        if root == directory:
+            continue
+
+        # Get the person's name from the folder path
+        person = os.path.basename(root)
+
+        # Create the train and test directories for this person
+        person_train_dir = os.path.join(train_dir, person)
+        person_test_dir = os.path.join(test_dir, person)
+        os.makedirs(person_train_dir, exist_ok=True)
+        os.makedirs(person_test_dir, exist_ok=True)
+        # Count the number of images for this person
+        image_count = len(files)
+        print("Processing", person, "with", image_count, "images")
+
+        # Split the images into train and test sets
+        train_count = int(image_count * 0.8)
+        test_count = image_count - train_count
+
+        # Iterate through the images
+        for i, file in enumerate(files):
+            # Get the file path
+            file_path = os.path.join(root, file)
+
+            # Extract the face from the image
+            face = extract_face(file_path, required_size=(img_width, img_height))
+
+            # If no face is detected, skip the image
+            if face is None:
+                print("No face detected in", file_path)
+                continue
+
+            # Preprocess the image
+            face = resize_image(face, size=(img_width, img_height))
+            face = convert_to_grayscale(face)
+            face = normalize_image(face)
+
+            # Decide whether to save to the train or test directory
+            if i < train_count:
+                save_image(face, person_train_dir, file)
+            else:
+                save_image(face, person_test_dir, file)
+
+        print("Finished processing", person)
+
+    print("Processing complete!")
+
+process_images()
